47c5f8163005ccaeac02d74d09a4116f3f82731c
AlexIA075 <163032623+irlepee@users.noreply.github.com>
2025-06-14 04:13:47 -0700

    Pre Cambio de Diseño

--- js/parallax.js
+++ js/parallax.js
@@ -2,4 +2,15 @@ window.addEventListener('scroll', function() {
     const parallax = document.querySelector('.parallax');
     let offset = window.scrollY * .5; // Ajusta el factor para más o menos movimiento
     parallax.style.backgroundPosition = `center ${offset}px`;
+
+    const parallax2 = document.querySelector('.parallax2');
+    if (parallax2) {
+        const sectionTop = parallax2.offsetTop;
+        let relativeScroll = window.scrollY - sectionTop;
+        if (relativeScroll > 0) {
+            parallax2.style.backgroundPosition = `center ${relativeScroll * 0.5}px`;
+        } else {
+            parallax2.style.backgroundPosition = 'center 0px';
+        }
+    }
 });
