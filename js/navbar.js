5f0e0b3928c9ab039b0f2ecb9c7560fabc01a3fe
AlexIA075 <163032623+irlepee@users.noreply.github.com>
2025-07-15 02:43:06 -0700

    Pre aparecer-ocultar navbar, probando el botón

--- js/navbar.js
+++ js/navbar.js
@@ -2,7 +2,9 @@ document.addEventListener("DOMContentLoaded", () => {
   fetch("/html/navbar.html")
     .then(res => res.text())
     .then(data => {
+
       document.getElementById("navbar-container").innerHTML = data;
+      
       const navbar = document.querySelector('.navbar');
       window.addEventListener('scroll', () => {
         if (window.scrollY > 400) {
@@ -11,5 +13,15 @@ document.addEventListener("DOMContentLoaded", () => {
           navbar.classList.remove('solid');
         }
       });
+
+      const navlist = document.querySelector('nav-list');
+      const navbarButton = document.getElementById("navbarbutton");
+      if (navbarButton && navlist) {
+        navbarButton.addEventListener("click", () => {
+          nav-list.classList.toggle("open");
+        });
+      }
     });
+
+
 });
